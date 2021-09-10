14b3f54775b2e88ab7d3997dc78ba5e139dea40c
Andrew <alo42@georgetown.edu>
2021-09-10 13:25:12 +0200

    initial commit

---
+++ boot_local.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo -e "Booting OpenQ devevelopment environment\n"
+echo -e "Assumes an Ethreum RPC node is running at localhost:8545\n"
+
+if [ -d "./contracts" ] 
+then
+    echo -e "/contracts directory found\n" 
+else
+    echo -e "No contracts directory found. Cloning contracts...\n"
+    git clone https://github.com/OpenQDev/contracts.git
+fi
+
+cd ./contracts
+yarn
+yarn deploy:local
+
+cd ..
+
+if [ -d "./OpenQ-API" ] 
+then
+    echo -e "\n/OpenQ-API directory found\n" 
+else
+    echo -e "No OpenQ-API directory found. Cloning OpenQ-API...\n"
+    git clone https://github.com/OpenQDev/OpenQ-API.git
+fi
+
+echo -e "Copying .env.docker file to OpenQ-API to connect to proper RPC node and contract addresses\n"
+cp ./contracts/.env.docker ./OpenQ-API/.env.docker
+cd ./OpenQ-API
+yarn
+docker compose up
