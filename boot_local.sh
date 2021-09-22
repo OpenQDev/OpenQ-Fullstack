5e9fd3843e032900dc2214d87f26e573a5b45cf6
satori <satori@pop-os.localdomain>
2021-09-22 17:03:03 +0200

    changed clone type to ssh

--- boot_local.sh
+++ boot_local.sh
@@ -6,7 +6,7 @@ then
     echo -e "/contracts directory found\n" 
 else
     echo -e "No contracts directory found. Cloning contracts...\n"
-    git clone https://github.com/OpenQDev/contracts.git
+    git clone git@github.com:OpenQDev/contracts.git
 fi
 
 cd ./contracts
@@ -28,7 +28,7 @@ then
     echo -e "OpenQ-API directory found\n" 
 else
     echo -e "No OpenQ-API directory found. Cloning OpenQ-API...\n"
-    git clone https://github.com/OpenQDev/OpenQ-API.git
+    git clone git@github.com:OpenQDev/OpenQ-API.git
 fi
 
 echo -e "Copying .env.docker file to OpenQ-API to connect to proper RPC node and contract addresses\n"
@@ -43,7 +43,7 @@ then
     echo -e "\n/frontend directory found\n" 
 else
     echo -e "No frontend directory found. Cloning frontend...\n"
-    git clone https://github.com/OpenQDev/frontend.git
+    git clone git@github.com:OpenQDev/frontend.git
 fi
 
 if [ $1 == "build" ]
