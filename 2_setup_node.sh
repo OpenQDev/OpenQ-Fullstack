ecd2c02c9a71d6dbc1968a1033e886f138681fd4
Andrew <aobrien@wolfram.com>
2021-09-28 00:59:41 +0200

    numbers deployment scripts for ordering

--- 2_setup_node.sh
+++ 2_setup_node.sh
@@ -5,5 +5,3 @@ echo -e "This will take 5 seconds..."
 echo -e "You can find the local nodes public/private keys in contracts/nohup.out\n"
 nohup yarn ethnode &
 sleep 5
-yarn deploy:local
-echo -e "Contracts deployed to Hardhat blockchain\n"
