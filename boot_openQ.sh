74e13af67a1196bd5da96a838a2d3e8b4004f110
Andrew <alo42@georgetown.edu>
2021-10-04 17:44:12 -0400

    checks if anything is running on port 8545

--- boot_openQ.sh
+++ boot_openQ.sh
@@ -16,6 +16,15 @@ chmod u+x 8_setup_fullstack.sh
 # Pull repos (if not present) and install dependencies
 ./1_setup_contracts.sh
 ./2_setup_node.sh
+# Exit if a process was running on port 8545
+if test $? -eq 1
+then
+    echo -e "See you soon!"
+    exit 1
+else
+	echo -e "Ethereum RPC Node deployed to port 8545. Proceeding..."
+fi
+
 ./3_deploy_contracts.sh
 ./4_setup_api.sh
 ./5_setup_frontend.sh
@@ -28,6 +37,17 @@ cp ./OpenQ-Contracts/.env.docker ./OpenQ-API/.env.docker
 echo -e "Copying .env.docker file to frontend .env to connect to proper RPC node and contract addresses\n"
 cp ./OpenQ-Contracts/.env.docker ./OpenQ-Frontend/.env
 cp -R ./OpenQ-Contracts/artifacts ./OpenQ-Frontend/
+
 cat PAT >> ./OpenQ-Frontend/.env
+if test $? -eq 1
+then
+    echo -e "${Red}You need to add a GitHub Personal Access Token (PAT) to a file simply called PAT if you want to continue.${Color_Off}"
+    echo -e "To get a PAT, follow the instructions here: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token"
+    echo -e "NOTE: You will need repo and org read access :-)"
+    lsof -ti tcp:8545 | xargs kill
+    exit 1
+else
+		echo -e "PAT copied into frontend. Proceeding..."
+fi
 
 ./8_setup_fullstack.sh $1
