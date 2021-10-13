e2730190ffb8ca1418d853c877ae432ae0098794
Andrew <alo42@georgetown.edu>
2021-10-13 15:17:56 -0400

    adds clone to file names

--- boot_openQ.sh
+++ boot_openQ.sh
@@ -3,23 +3,22 @@
 chmod u+x asciiart.sh
 ./asciiart.sh
 
-# Grant execution permissions to bootscript
-chmod u+x 1_contracts.sh
-chmod u+x 2_api.sh
-chmod u+x 3_frontend.sh
-chmod u+x 4_helm.sh
-chmod u+x 5_kubeconfig.sh
-chmod u+x 6_github_oauth_server.sh
-chmod u+x 7_cert_manager.sh
-chmod u+x 8_json_rpc_node.sh
-chmod u+x 9_fullstack.sh
+chmod u+x 1_clone_contracts.sh
+chmod u+x 2_clone_api.sh
+chmod u+x 3_clone_frontend.sh
+chmod u+x 4_clone_helm.sh
+chmod u+x 5_clone_kubeconfig.sh
+chmod u+x 6_clone_github_oauth_server.sh
+chmod u+x 7_clone_cert_manager.sh
+chmod u+x 8_clone_json_rpc_node.sh
+chmod u+x 9_compose_fullstack.sh
 
-./1_contracts.sh
-./2_api.sh
-./3_frontend.sh
-./4_helm.sh
-./5_kubeconfig.sh
-./6_github_oauth_server.sh
-./7_cert_manager.sh
-./8_json_rpc_node.sh
-./9_fullstack.sh $1
+./1_clone_contracts.sh
+./2_clone_api.sh
+./3_clone_frontend.sh
+./4_clone_helm.sh
+./5_clone_kubeconfig.sh
+./6_clone_github_oauth_server.sh
+./7_clone_cert_manager.sh
+./8_clone_json_rpc_node.sh
+./9_compose_fullstack.sh $1
