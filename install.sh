2ad90236e8145d813f89265ac20b26369079f5c0
mktcode <kontakt@markus-kottlaender.de>
2023-01-20 16:44:47 +0100

    split boot.sh into install and boot

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+. ./clone.sh
+. ./colors.sh
+chmod u+x asciiart.sh
+./asciiart.sh
+chmod u+x env.sh
+. ./env.sh
+
+declare -a repos=(
+	"OpenQ-CoinAPI"
+	"OpenQ-Oracle"
+	"OpenQ-Frontend"
+	"OpenQ-Contracts"
+	"OpenQ-Github-OAuth-Server"
+	"OpenQ-Helm"
+	"OpenQ-JSON-RPC-Node"
+	"OpenQ-CertManager"
+	"OpenQ-Graph"
+	"OpenQ-Kubeconfig"
+	"OpenQ-Bot"
+	"OpenQ-API"
+	"OpenQ-Architecture"
+	"OpenQ-Github-Proxy"
+	"OpenQ-Email-Server"
+	"OpenQ-OZ-Claim-Autotask"
+	"OpenQ-Bounty-Actions-Autotask"
+	"OpenQ-Event-Listener"
+	"OpenQ-Token-Price-Cron-Job"
+	"Superfluid-Subgraph"
+	"OpenQ-User-Registration"
+)
+
+for repo in "${repos[@]}"; do
+	echo -e "\n${BBlue}$repo${Color_Off}"
+	clone "$repo"
+	generate_dot_env "$repo"
+done
