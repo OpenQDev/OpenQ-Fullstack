015078b3113c0b3bdf9b7d7106be691af908d0ca
FlacoJones <andrew@openq.dev>
2023-02-20 16:16:36 -0600

    increases COMPOSE timeout for longer boot time on some machines

--- boot.sh
+++ boot.sh
@@ -4,4 +4,4 @@ echo -e ${Cyan}"Starting all OpenQ containers from scratch..."${Color_Off}
 docker stop $(docker ps -aq) && docker rm $(docker ps -aq)
 echo -e ${Cyan}"Removed stale OpenQ containers. Booting new..."${Color_Off}
 
-docker-compose -f docker-compose.yml up
+COMPOSE_HTTP_TIMEOUT=200 docker-compose -f docker-compose.yml up
