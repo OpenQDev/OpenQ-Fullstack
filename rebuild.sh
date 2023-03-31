6caefc80be66bc279c8d0e89f48f6fc432638dce
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2023-03-31 15:16:29 +0000

    updated rebuild script and added env to gitignore

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,5 @@
 containername=$1
-docker stop containername
-docker rm containername
+docker stop $containername
+docker rm $containername
 docker-compose build --no-cache $containername $containername
 docker-compose up --build --force-recreate --no-deps -d $containername
